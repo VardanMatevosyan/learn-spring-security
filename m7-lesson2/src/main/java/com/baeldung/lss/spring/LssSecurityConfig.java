a2a1a3f062e65edfa784309f72c24cfb37043d29
VardanMatevosyan <vardanmt@gmail.com>
2023-08-27 02:03:53 +0300

    Module 7 - Security advance configuration finished 2 lesson  1. Run as different user using runAsManager and runAsImplAuthenticationProvider  2.  configuring few providers with global method security

--- m7-lesson2/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m7-lesson2/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -3,11 +3,17 @@ package com.baeldung.lss.spring;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.intercept.RunAsImplAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,20 +25,22 @@ import com.baeldung.lss.persistence.UserRepository;
 @Configuration
 public class LssSecurityConfig {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final UserRepository userRepository;
+    private final AuthenticationProvider daoAuthenticationProvider;
+    private final AuthenticationProvider runAsAuthProvider;
+    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    private PasswordEncoder passwordEncoder;
-
-    public LssSecurityConfig(PasswordEncoder passwordEncoder) {
+    public LssSecurityConfig(
+        UserRepository userRepository,
+        @Qualifier("daoAuthenticationProvider") AuthenticationProvider daoAuthenticationProvider,
+        @Qualifier("runAsAuthProvider") AuthenticationProvider runAsAuthProvider,
+        PasswordEncoder passwordEncoder) {
         super();
+        this.userRepository = userRepository;
+        this.daoAuthenticationProvider = daoAuthenticationProvider;
+        this.runAsAuthProvider = runAsAuthProvider;
         this.passwordEncoder = passwordEncoder;
     }
-
-    //
 
     @PostConstruct
     private void saveTestUser() {
@@ -44,7 +52,8 @@ public class LssSecurityConfig {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {// @formatter:off
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        auth.authenticationProvider(runAsAuthProvider);
+        auth.authenticationProvider(daoAuthenticationProvider);
     } // @formatter:on
 
     @Bean
